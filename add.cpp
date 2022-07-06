c75d9483b14bdd7d0aff9aee0afc2bb162d268c1
MUAHMMEDZAMILC <mzofficial049@gmail.com>
2022-07-06 10:19:12 +0530

    update 0

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ int sum=0;
+ cout<<"enter the first number:";
+ cin>>a;
+ cout<<"enter the second number:";
+ cin>>b;
+ sum=a+b;
+ cout<<"sum of given two  numbers is:"<<sum;
+ return 0;
+}
