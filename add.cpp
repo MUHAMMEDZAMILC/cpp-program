135fe3a57358fdb8d4e4bcf45f03cf5cc897af3f
MUAHMMEDZAMILC <mzofficial049@gmail.com>
2022-07-06 10:40:51 +0530

    update 1

--- add.cpp
+++ add.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
- int a,b;
+ int a,b,c;
  int sum=0;
  cout<<"enter the first number:";
  cin>>a;
  cout<<"enter the second number:";
  cin>>b;
+ cout<<"enter the third number:";
+ cin>>c;
  sum=a+b;
  cout<<"sum of given two  numbers is:"<<sum;
  return 0;
